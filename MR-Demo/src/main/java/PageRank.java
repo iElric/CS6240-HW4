b3f915675aa7ca29c46fb0916cf7955ac17d92f3
Yexin Wang <wang.yexin@outlook.com>
2019-11-03 20:51:15 -0500

    Almost finish mr.

--- MR-Demo/src/main/java/PageRank.java
+++ MR-Demo/src/main/java/PageRank.java
@@ -1,21 +1,9 @@
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Counter;
-import org.apache.hadoop.mapreduce.Counters;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -31,24 +19,117 @@ public class PageRank extends Configured implements Tool {
   public static class GraphMapper extends Mapper<Object, Text, Text, Text> {
 
     @Override
-    public void map(final Object key, final Text value, final Context context)
+    public void map(Object key, Text value, Context context)
         throws IOException, InterruptedException {
       double initPR = 1.0 / (k * k);
       String val = null;
       for (int i = 1; i <= k * k; i++) {
         if (i % k == 0) {
-          // the node who has no out edge, which is dangling page, all set to 0
+          // the node who has no out edge, which is dangling page, all set to 0 as out
           val = 0 + "," + initPR;
         } else {
-          val = i + 1 + "," + initPR;
+          int j = i + 1;
+          val = j + "," + initPR;
         }
         context.write(new Text(Integer.toString(i)), new Text(val));
       }
-      val = "all|" + 0;
+      // this dummy should output to all other nodes
+      val = "dummy" + "," + 0;
       context.write(new Text(Integer.toString(0)), new Text(val));
     }
   }
 
+  /**
+   * Map process on vertex n which contains current page rank and its adj list.
+   */
+  public static class PRMapper extends Mapper<Object, Text, Text, Text> {
+
+    @Override
+    public void map(Object key, Text value, Context context)
+        throws IOException, InterruptedException {
+      String[] strings = value.toString().split("\t");
+      // emit the graph structure
+      context.write(new Text(strings[0]), new Text(strings[1]));
+      String[] vals = strings[1].split(",");
+
+      double pr = Double.parseDouble(vals[1]);
+      if (vals[0].equals("dummy")) {
+        // send to all other nodes
+        for (int i = 1; i <= k * k; i++) {
+          context.write(new Text(Integer.toString(i)), new Text(Double.toString(pr / (k * k))));
+        }
+      } else {
+        // emit to all node in adj list, in this case just one.
+        context.write(new Text(vals[0]), new Text(Double.toString(pr)));
+      }
+
+    }
+  }
+
+  // same as the module pseudo code
+  public static class PRReducer extends Reducer<Text, Text, Object, Object> {
+
+    @Override
+    public void reduce(Text key, Iterable<Text> values, Context context)
+        throws IOException, InterruptedException {
+      String adjNode = null;
+      double sum = 0.0;
+      for (Text val : values) {
+        String[] strings = val.toString().split(",");
+        // when this is a graph structure
+        if (strings.length > 1) {
+          // get the adjust node, abandon strings[1] which is the old pr val
+          adjNode = strings[0];
+        } else {
+          sum += Double.parseDouble(val.toString());
+        }
+      }
+      // update the pr value and keep the graph
+      sum = 0.15 / k * k + 0.85 * sum;
+      // restore the graph structure with adjNode amd the new pr value
+      context.write(key, new Text(adjNode + "," + sum));
+    }
+  }
+
+  /**
+   * Extract the dummy 0 pr and send to others since the final result should not contain 0 node.
+   */
+  public static class finalMapper extends Mapper<Object, Text, Text, Text> {
+
+    @Override
+    public void map(Object key, Text value, Context context)
+        throws IOException, InterruptedException {
+      String[] strings = value.toString().split("\t");
+      String[] vals = strings[1].split(",");
+      double pr = Double.parseDouble(vals[1]);
+      if (strings[0].equals(Integer.toString(0))) {
+        for (int i = 1; i <= k * k; i++) {
+          // dummy gives all pr away
+          context.write(new Text(Integer.toString(i)), new Text(Double.toString(pr / (k * k))));
+        }
+      } else {
+        // map the original pr
+        context.write(new Text(strings[0]), new Text(Double.toString(pr)));
+      }
+    }
+  }
+
+  /**
+   * Sum the pr gave out by dummy. Values should contain just two objects.
+   */
+  public static class finalReducer extends Reducer<Text, Text, Object, Object> {
+    @Override
+    public void reduce(Text key, Iterable<Text> values, Context context)
+        throws IOException, InterruptedException {
+      double sum = 0.0;
+      for (Text val : values) {
+        sum += Double.parseDouble(val.toString());
+      }
+      context.write(key, new Text(Double.toString(sum)));
+    }
+  }
+
   
+
 
 }
